3c45919300ca49484050d666f0ee49c723dc892f
1918686415@qq.com <1918686415@qq.com>
2023-07-13 15:28:23 +0800

    Initial commit

---
+++ src/main/java/LanZhou/App.java
@@ -0,0 +1,13 @@
+package LanZhou;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
